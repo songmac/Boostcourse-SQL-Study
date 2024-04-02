ba6f6e358096f1c7433c4619d26e885f5f6745e5
songmac <ssy4784@naver.com>
2024-04-03 03:20:53 +0900

    Update : practice of select rules

--- Code/10_데이터 조회(SELECT)_practice.sql
+++ Code/10_데이터 조회(SELECT)_practice.sql
@@ -1,115 +1,100 @@
+-- DB 사용 선언
 USE PRACTICE;
 
-/***************FROM***************/
+-- 데이터 조회를 위한 명령어 순서
+--(계속) SELECT -> WHERE -> WHERE -> GROUP BY -> HAVING -> ORDER BY
 
-/* Customer 테이블 모든 열 조회 */
+
+
+
+-- FROM
+-- Customer 테이블 모든 열 조회
 SELECT  *
   FROM  CUSTOMER;
 
 
-/***************WHERE***************/
 
-/* 성별이 남성 조건으로 필터링 */
+-- WHERE
+-- 성별이 남성인 조건으로 필터링
 SELECT  *
   FROM  CUSTOMER
  WHERE  GENDER = 'MAN';
  
  
-/***************GROUP BY***************/
 
-/* 지역별로 회원수 집계 */
+-- GROUP BY : GROUP BY 컬럼명과 SELECT 컬럼명이 같아야 함
+-- COUNT : 행들의 개수를 구하는 집계함수
+-- 지역별로 회원수 집계
 SELECT  ADDR
-		,COUNT(MEM_NO) AS 회원수
+		  ,COUNT(MEM_NO) AS 회원수
   FROM  CUSTOMER
  WHERE  GENDER = 'MAN'
- GROUP
-    BY  ADDR;
+ GROUP BY  ADDR;
     
-/* COUNT: 행들의 개수를 구하는 집계함수 */
 
-
-/***************HAVING***************/
-
-/* 집계 회원수 100명 미만 조건으로 필터링 */
+-- HAVING
+-- 집계 회원수 100명 미만 조건으로 필터링
 SELECT  ADDR
-		,COUNT(MEM_NO) AS 회원수
+		  ,COUNT(MEM_NO) AS 회원수
   FROM  CUSTOMER
  WHERE  GENDER = 'MAN'
- GROUP
-    BY  ADDR
+ GROUP BY  ADDR
 HAVING  COUNT(MEM_NO) < 100;
     
-/* < : 비교 연산자 / ~ 보다 작은*/
 
 
-/***************ORDER BY***************/
-
-/* 집계 회원수가 높은 순으로 */
+-- ORDER BY(DESC : 내림차순, ASC(기본) : 오름차순)
+-- 집계 회원수가 높은 순으로 정렬
 SELECT  ADDR
-		,COUNT(MEM_NO) AS 회원수
+		  ,COUNT(MEM_NO) AS 회원수
   FROM  CUSTOMER
  WHERE  GENDER = 'MAN'
- GROUP
-    BY  ADDR
+ GROUP BY  ADDR
 HAVING  COUNT(MEM_NO) < 100
- ORDER
-	BY  COUNT(MEM_NO) DESC;
+ ORDER BY  COUNT(MEM_NO) DESC;
     
-/* DESC : 내림차순 / ASC : 오름차순 */
 
 
-/***************FROM -> (WHERE) -> GROUP BY***************/
 
-/* FROM -> GROUP BY 순으로 작성해도 됩니다. */
+-- FROM -> (WHERE) -> GROUP BY
+-- FROM -> GROUP BY 순으로 작성해도 됨
 SELECT  ADDR
 		,COUNT(MEM_NO) AS 회원수
   FROM  CUSTOMER
 /* WHERE  GENDER = 'MAN' */
- GROUP
-    BY  ADDR;
+ GROUP BY  ADDR;
 
 
-/***************GROUP BY + 집계함수***************/
-/* 거주지역을 서울, 인천 조건으로 필터링 */
-/* 거주지역 및 성별로 회원수 집계 */
+
+-- GROUP BY + 집계함수
+-- IN : 특수 연산자로 IN (List) 안에 리스트 값만 사용
+-- 거주지역을 서울, 인천 조건으로 필터링
+-- 거주지역 및 성별로 회원수 집계
 SELECT  ADDR
-		,GENDER
-		,COUNT(MEM_NO) AS 회원수
+		  ,GENDER
+		  ,COUNT(MEM_NO) AS 회원수
   FROM  CUSTOMER
  WHERE  ADDR IN ('SEOUL', 'INCHEON')
- GROUP
-    BY  ADDR
-		,GENDER;
+ GROUP BY  ADDR
+		     ,GENDER;
 
-/* IN : 특수 연산자 / IN (List) / 리스트 값만 */
 
-/* GROUP BY에 있는 열들을 SELECT에도 작성해야 원하는 분석 결과를 확인할 수 있습니다. */
-SELECT  GENDER
-		,COUNT(MEM_NO) AS 회원수
-  FROM  CUSTOMER
- WHERE  ADDR IN ('SEOUL', 'INCHEON')
- GROUP
-    BY  ADDR
-		,GENDER;
-        
 
-/***************SQL 명령어 작성법***************/
-/* 회원테이블(Customer)을 */
-/* 성별이 남성 조건으로 필터링하여 */
-/* 거주지역별로 회원수 집계 */
-/* 집계 회원수 100명 미만 조건으로 필터링 */
-/* 모든 열 조회 */
-/* 집계 회원수가 높은 순으로 */
+-- SQL 명령어 작성법 정리
+-- (계속) 회원테이블(Customer)을
+-- (계속) 성별이 남성 조건으로 필터링하여
+-- (계속) 거주지역별로 회원수 집계
+-- (계속) 집계 회원수 100명 미만 조건으로 필터링
+-- (계속) 모든 열 조회
+-- (계속) 집계 회원수가 높은 순으로 정렬
 
 SELECT  ADDR
 		,COUNT(MEM_NO) AS 회원수
   FROM  CUSTOMER
  WHERE  GENDER = 'MAN'
- GROUP
-    BY  ADDR
+ GROUP BY  ADDR
 HAVING  COUNT(MEM_NO) < 100
- ORDER
-    BY  COUNT(MEM_NO) DESC;
+ ORDER BY  COUNT(MEM_NO) DESC;
 
 
 
